0beaa44ba5a01f5745afd5aade0c829dfc87ac4a
silvicardo <silvicardo@gmail.com>
2018-11-20 15:33:37 +0100

    Now you can select your difficulty level, functions, loops  and switch statement

--- main.js
+++ main.js
@@ -1,63 +1,97 @@
-// EASY CAMPO MINATO
+//CAMPO MINATO CON DIFFICOLTA'
 
-var numeriVietati = [];
+var livelloDiDifficolta = impostaLivelloDiDifficolta();
 
-var maxNumeriVietati = 16;
-
-numeriVietati.push(generaNumeroPositivoCon(100));
-
-for (var i = 1; i < maxNumeriVietati; i++) {
-
-  var numeroEstratto = generaNumeroPositivoCon(100);
-
-  if (numeriVietati.indexOf(numeroEstratto) == -1) {
-    numeriVietati.push(numeroEstratto);
-  } else {
-    i--;
-  }
-}
-
-console.log(numeriVietati);
-
-alert("CAMPO MINATO.\nIl computer genera 16 numeri casuali da 1 a 100.\nDovrai inserire un numero da 1 a 100 alla volta, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti potrai inserire un altro numero.\nLa partita termina quando inserisci un numero vietato o raggiungi il numero massimo possibile di numeri consentiti.\nAl termine della partita vedrai il tuo punteggio.");
-
-var numeriEstrattiDallUtente = [];
-var punteggio = 0;
-var puoiContinuare = true;
-
-while (numeriEstrattiDallUtente.length <= 84 && puoiContinuare) {
-
-  var messaggioUtente;
-
-  if (numeriEstrattiDallUtente.length > 0) {
-    messaggioUtente = 'Finora hai estratto: ' + numeriEstrattiDallUtente + '\nIl tuo punteggio è: ' + punteggio + '\nInserisci il tuo numero da 1 a 100';
-  } else {
-    messaggioUtente = 'Che il gioco inizi!\nBuona Fortuna\nInserisci il tuo numero da 1 a 100';
-  }
-
-  var numeroUtente = parseInt(prompt(messaggioUtente));
-
-  if (numeroUtente > 0 && numeroUtente <= 100){
-    if (numeriVietati.indexOf(numeroUtente) == -1) {
-      if (numeriEstrattiDallUtente.indexOf(numeroUtente) == -1) {
-        numeriEstrattiDallUtente.push(numeroUtente);
-        console.log(numeriEstrattiDallUtente);
-        punteggio++;
-      } else {
-        alert('Hai già estratto il numero ' + numeroUtente);
-      }
-    } else {
-      puoiContinuare = false;
-    }
-  } else {
-    alert('Hai Inserito un numero non Valido!');
-  }
-
-}
-
-alert('hai perso con un punteggio di: ' + punteggio );
+giocaCon(livelloDiDifficolta);
 
 // FUNZIONI
 function generaNumeroPositivoCon(maxNum) {
   return Math.ceil(Math.random() * maxNum);
+}
+
+function impostaLivelloDiDifficolta() {
+  return parseInt(prompt('Inserisci un livello di difficoltà:\n-0(Facile: scegli numeri da 1 a 100)\n-1(Medio: scegli numeri da 1 a 80)\n-2(Difficile: scegli numeri da 1 a 50)'));
+}
+
+function generaNumeriVietatiPer(numeroMassimoEstraibile) {
+
+  var maxNumeriVietati = 16;
+  var numeriVietati = [];
+
+  for (var i = 0; i < maxNumeriVietati; i++) {
+
+    var numeroEstratto = generaNumeroPositivoCon(numeroMassimoEstraibile);
+
+    if (numeriVietati.indexOf(numeroEstratto) == -1) {
+      numeriVietati.push(numeroEstratto);
+    } else {
+      i--;
+    }
+  }
+
+  console.log(numeriVietati);
+
+  return numeriVietati;
+}
+
+function numeroMassimoEstraibilePer(livelloDiDifficolta) {
+
+  var numeroMassimo;
+
+  switch (livelloDiDifficolta) {
+    case 0 : numeroMassimo = 100;
+    break;
+    case 1 : numeroMassimo = 80;
+    break;
+    case 2 : numeroMassimo = 50;
+    break;
+  }
+
+  return numeroMassimo;
+}
+
+function giocaCon(livelloDiDifficolta) {
+
+  var numeroMassimoEstraibile = numeroMassimoEstraibilePer(livelloDiDifficolta);
+
+  alert('CAMPO MINATO.\nLivello Di Difficoltà: ' + livelloDiDifficolta + '\nIl computer genera 16 numeri casuali da 1 a ' + numeroMassimoEstraibile + '.\nDovrai inserire un numero da 1 a ' + numeroMassimoEstraibile + ' alla volta, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti potrai inserire un altro numero.\nLa partita termina quando inserisci un numero vietato o raggiungi il numero massimo possibile di numeri consentiti.\nAl termine della partita vedrai il tuo punteggio.');
+
+  var numeriVietati = generaNumeriVietatiPer(numeroMassimoEstraibile);
+  var numeriCorrettiEstraibiliDallUtente = numeroMassimoEstraibile - 16;
+  var numeriEstrattiDallUtente = [];
+  var punteggio = 0;
+  var puoiContinuare = true;
+
+  while (numeriEstrattiDallUtente.length <= numeriCorrettiEstraibiliDallUtente && puoiContinuare) {
+
+    var messaggioUtente;
+
+    if (numeriEstrattiDallUtente.length > 0) {
+      messaggioUtente = 'Finora hai estratto: ' + numeriEstrattiDallUtente + '\nIl tuo punteggio è: ' + punteggio + '\nInserisci il tuo numero da 1 a ' + numeroMassimoEstraibile;
+    } else {
+      messaggioUtente = 'Che il gioco inizi!\nBuona Fortuna\nInserisci il tuo numero da 1 a ' + numeroMassimoEstraibile;
+    }
+
+    var numeroUtente = parseInt(prompt(messaggioUtente));
+
+    if (numeroUtente > 0 && numeroUtente <= numeroMassimoEstraibile){
+      if (numeriVietati.indexOf(numeroUtente) == -1) {
+        if (numeriEstrattiDallUtente.indexOf(numeroUtente) == -1) {
+          numeriEstrattiDallUtente.push(numeroUtente);
+          console.log(numeriEstrattiDallUtente);
+          punteggio++;
+        } else {
+          alert('Hai già estratto il numero ' + numeroUtente);
+        }
+      } else {
+        puoiContinuare = false;
+      }
+    } else {
+      alert('Hai Inserito un numero non Valido!');
+    }
+
+  }
+
+  alert('hai perso selezionando: ' + numeroUtente + '\nPunteggio di: ' + punteggio );
+
 }
